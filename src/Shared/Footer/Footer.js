00a1a9adc60bc438cafd7001c351bb6bfb8e3ea2
tantaana <shariartanvirofficial@gmail.com>
2023-11-10 01:37:13 +0600

    footer is done

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,7 +1,97 @@
 import React from "react";
+import styles from "./Footer.module.css";
+
+import { FaFacebookSquare, FaLinkedinIn } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
+
+import logo from "../../Assets/Navbar/logo.png";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="bg-black">
+      <div className="grid grid-cols-3 gap-4 px-32 py-32">
+        <div className="flex justify-center">
+          <div>
+            <img src={logo} className="w-[120px] mb-10" alt="" />
+
+            <h2 className={`${styles.footerText} text-white mb-1`}>
+              Need any help?
+            </h2>
+            <h2 className="text-white text-xl font-semibold mb-6">
+              Call Us: <span className="text-yellow-400">(+880)1628672468</span>
+            </h2>
+
+            <h2 className="text-white mb-2">
+              223, Firinghee Bazar, Chittagong, Bangladesh
+            </h2>
+            <h2 className="text-white mb-8">wanderwoman.bd@gmail.com</h2>
+
+            <div className="flex items-center gap-4">
+              <FaFacebookSquare className="text-white text-2xl hover:cursor-pointer" />
+              <BsTwitter className="text-white text-2xl hover:cursor-pointer" />
+              <FaLinkedinIn className="text-white text-2xl hover:cursor-pointer" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center border-l border-r border-gray-600">
+          <div className="grid grid-cols-2 gap-16">
+            <div className="flex flex-col items-start">
+              <h2 className={`${styles.footerText} text-white mb-6`}>
+                Company
+              </h2>
+
+              <h2 className="text-white mb-3 hover:cursor-pointer">About Us</h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">
+                Contact Us
+              </h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">
+                Travel Guides
+              </h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">
+                Data Policy
+              </h2>
+            </div>
+
+            <div className="flex flex-col items-start">
+              <h2 className={`${styles.footerText} text-white mb-6`}>
+                Top Destination
+              </h2>
+
+              <h2 className="text-white mb-3 hover:cursor-pointer">
+                Las Vegas
+              </h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">New York</h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">
+                San Francisco
+              </h2>
+              <h2 className="text-white mb-3 hover:cursor-pointer">Hawaii</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col items-start">
+            <h2 className={`${styles.footerText} text-white mb-6`}>
+              Sign Up Newsletter
+            </h2>
+
+            <input
+              type="email"
+              placeholder="Enter email.."
+              className="input input-ghost w-full max-w-xs bg-white mb-4"
+            />
+
+            <h2 className="text-white font-semibold bg-yellow-600 hover:bg-yellow-800 duration-300 ease-in-out px-6 py-2 rounded-lg hover:cursor-pointer">
+              Submit
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-yellow-400 w-full h-[3px]"></div>
+    </div>
+  );
 };
 
 export default Footer;
